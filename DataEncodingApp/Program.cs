1cd26203e886c71cfaca7d5706f116d737007fe2
Александр Сапунов <sapunov032@gmail.com>
2024-03-03 17:48:27 +0800

    Добавьте файлы проекта.

---
+++ DataEncodingApp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static DataEncodingApp.ShannonFano;
+
+namespace DataEncodingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Symbol> symbols = new List<Symbol>
+            {
+                new Symbol { Character = "1", Probability = 0.3, Code = "" },
+                new Symbol { Character = "2", Probability = 0.2, Code = "" },
+                new Symbol { Character = "3", Probability = 0.15, Code = "" },
+                new Symbol { Character = "4", Probability = 0.1, Code = "" },
+                new Symbol { Character = "5", Probability = 0.1, Code = "" },
+                new Symbol { Character = "6", Probability = 0.05, Code = "" },
+                new Symbol { Character = "7", Probability = 0.05, Code = "" },
+                new Symbol { Character = "8", Probability = 0.03, Code = "" },
+                new Symbol { Character = "9", Probability = 0.02, Code = "" },
+
+            };
+
+            List<Symbol> symbolAlpavit = new List<Symbol>
+            {
+                new Symbol { Character = " ", Probability = 0.145, Code = "" },
+                new Symbol { Character = "о", Probability = 0.095, Code = "" },
+                new Symbol { Character = "е", Probability = 0.074, Code = "" },
+                new Symbol { Character = "а", Probability = 0.064, Code = "" },
+                new Symbol { Character = "и", Probability = 0.064, Code = "" },
+                new Symbol { Character = "т", Probability = 0.056, Code = "" },
+                new Symbol { Character = "н", Probability = 0.056, Code = "" },
+                new Symbol { Character = "с", Probability = 0.047, Code = "" },
+                new Symbol { Character = "р", Probability = 0.041, Code = "" },
+                new Symbol { Character = "в", Probability = 0.039, Code = "" },
+                new Symbol { Character = "л", Probability = 0.036, Code = "" },
+                new Symbol { Character = "к", Probability = 0.029, Code = "" },
+                new Symbol { Character = "м", Probability = 0.026, Code = "" },
+                new Symbol { Character = "д", Probability = 0.026, Code = "" },
+                new Symbol { Character = "п", Probability = 0.024, Code = "" },
+                new Symbol { Character = "у", Probability = 0.021, Code = "" },
+                new Symbol { Character = "я", Probability = 0.019, Code = "" },
+                new Symbol { Character = "ы", Probability = 0.016, Code = "" },
+                new Symbol { Character = "з", Probability = 0.015, Code = "" },
+                new Symbol { Character = "ь,ъ", Probability = 0.015, Code = "" },
+                new Symbol { Character = "б", Probability = 0.015, Code = "" },
+                new Symbol { Character = "г", Probability = 0.014, Code = "" },
+                new Symbol { Character = "ч", Probability = 0.013, Code = "" },
+                new Symbol { Character = "й", Probability = 0.010, Code = "" },
+                new Symbol { Character = "х", Probability = 0.009, Code = "" },
+                new Symbol { Character = "ж", Probability = 0.008, Code = "" },
+                new Symbol { Character = "ю", Probability = 0.007, Code = "" },
+                new Symbol { Character = "ш", Probability = 0.006, Code = "" },
+                new Symbol { Character = "ц", Probability = 0.004, Code = "" },
+                new Symbol { Character = "щ", Probability = 0.003, Code = "" },
+                new Symbol { Character = "э", Probability = 0.003, Code = "" },
+                new Symbol { Character = "ф", Probability = 0.002, Code = "" }
+            };
+
+            ShannonFano sf = new ShannonFano();
+            sf.Encode(symbolAlpavit.OrderByDescending(x => x.Probability).ToList());
+            sf.Show(symbolAlpavit);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DataEncodingApp/ShannonFano.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataEncodingApp
+{
+    public class ShannonFano
+    {
+        public class Symbol
+        {
+            public string Character { get; set; }
+            public double Probability { get; set; }
+            public string Code { get; set; }
+        }
+
+        public List<Symbol> Symbols { get; set; }
+
+        public ShannonFano()
+        {
+
+        }
+
+        public void Encode(List<Symbol> symbols)
+        {
+            if (symbols.Count <= 1)
+                return;
+
+            double halfSum = symbols.Sum(s => s.Probability) / 2;
+            double sum = 0;
+
+            List<Symbol> firstList = new List<Symbol>();
+            List<Symbol> secondList = new List<Symbol>();
+
+            foreach(var item in symbols)
+            {
+                if (sum + item.Probability <= halfSum || firstList.Count==0)
+                    firstList.Add(item);
+                else
+                    secondList.Add(item);
+                sum += item.Probability;
+            }
+
+            _addCode(firstList, "0");
+            _addCode(secondList, "1");
+            //Console.WriteLine("First list");
+           // Show(firstList);
+            //Console.WriteLine("Second list");
+            //Show(secondList);
+
+            Encode(firstList);
+            Encode(secondList);
+        }
+
+        public void SplitSymbols(List<Symbol> symbols)
+        {
+            if (symbols.Count <= 1)
+            {
+                return;
+            }
+
+            double totalProbability = symbols.Sum(s => s.Probability);
+            double currentSum = 0;
+            double halfSum = totalProbability / 2;
+
+            var firstHalf = new List<Symbol>();
+            var secondHalf = new List<Symbol>();
+
+            foreach (var symbol in symbols)
+            {
+                if (currentSum + symbol.Probability <= halfSum)
+                {
+                    firstHalf.Add(symbol);
+                }
+                else
+                {
+                    secondHalf.Add(symbol);
+                }
+                currentSum += symbol.Probability;
+            }
+
+            foreach (var symbol in firstHalf)
+            {
+                symbol.Code += "0";
+            }
+
+            foreach (var symbol in secondHalf)
+            {
+                symbol.Code += "1";
+            }
+
+            SplitSymbols(firstHalf);
+            SplitSymbols(secondHalf);
+        }
+
+        private void _addCode(List<Symbol> symbols,string ncode)
+        {
+            foreach (var symbol in symbols)
+            {
+                symbol.Code += ncode;
+            }
+        }
+
+        public void Show(List<Symbol> list)
+        {
+            foreach (var item in list)
+                Console.WriteLine($"Char:{item.Character}\tProp:{item.Probability}\tCode{item.Code}");
+        }
+
+    }
+}
